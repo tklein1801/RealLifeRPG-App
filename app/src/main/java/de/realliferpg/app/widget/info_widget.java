7b4ef3473d2720e54f40ca26f298cdb1006039b8
Leopold Pöchinger <poechinger@gmail.com>
2018-07-16 01:04:53 +0200

    Widget onResponse

--- app/src/main/java/de/realliferpg/app/widget/info_widget.java
+++ app/src/main/java/de/realliferpg/app/widget/info_widget.java
@@ -1,43 +1,31 @@
 package de.realliferpg.app.widget;
 
-import android.app.AlertDialog;
-import android.app.PendingIntent;
+
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.RemoteViews;
 
 import com.google.gson.Gson;
-
-import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
 
-import de.realliferpg.app.R;
 import de.realliferpg.app.Singleton;
-import de.realliferpg.app.adapter.ServerListAdapter;
 import de.realliferpg.app.helper.ApiHelper;
 import de.realliferpg.app.interfaces.RequestCallbackInterface;
 import de.realliferpg.app.objects.Server;
-import de.realliferpg.app.Singleton.*;
+import de.realliferpg.app.R;
 
-public class info_widget extends AppWidgetProvider implements RequestCallbackInterface{
+
+
+public class info_widget extends AppWidgetProvider implements RequestCallbackInterface {
 
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
 
-        final ApiHelper apiHelper = new ApiHelper(this);
+        ApiHelper apiHelper = new ApiHelper(this);
         apiHelper.getServers();
 
         Object response = Singleton.getInstance().getServerinfo();
@@ -53,15 +41,17 @@ public class info_widget extends AppWidgetProvider implements RequestCallbackInt
 
             final ArrayList<Server> servers = new ArrayList<>(Arrays.asList(value.data));
 
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.info_widget);
 
 
+            remoteViews.setTextViewText(R.id.lv_widget_serverList, servers.get(1).Description);
 
 
-
+        }
     }
 
-    @Override
-    public void onResponse(Object response, Class type) {
+        @Override
+        public void onResponse(Object response, Class type) {
 
 
         Singleton.getInstance().setServerinfo(response);
